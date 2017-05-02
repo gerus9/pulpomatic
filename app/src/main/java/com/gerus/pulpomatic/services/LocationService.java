bfa136b3104fda8508a64197e91c0b6059a976db
Gerus-Mac <gerardo.guzmand@gmail.com>
2017-05-02 03:06:46 -0500

    Change rules distance

--- app/src/main/java/com/gerus/pulpomatic/services/LocationService.java
+++ app/src/main/java/com/gerus/pulpomatic/services/LocationService.java
@@ -3,35 +3,40 @@ package com.gerus.pulpomatic.services;
 /**
  * Created by gerus-mac on 28/04/17.
  */
-
-import android.app.ActivityManager;
+import android.Manifest;
 import android.app.Service;
-import android.content.Context;
 import android.content.Intent;
 import android.content.ServiceConnection;
+import android.content.pm.PackageManager;
 import android.location.Location;
-import android.location.LocationManager;
 import android.os.Binder;
 import android.os.Bundle;
 import android.os.IBinder;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.util.Log;
 
-import com.gerus.pulpomatic.notifications.Notifications;
 import com.gerus.pulpomatic.models.RulesVO;
+import com.gerus.pulpomatic.notifications.Notifications;
 import com.gerus.pulpomatic.sharedPreferences.MapsSP;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.model.LatLng;
 
-import java.util.List;
+public class LocationService extends Service implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, LocationListener {
 
-public class LocationService extends Service {
-
-    public static final int LOCATION_INTERVAL_DEFAULT = 500;  // 10 min
-    public static final float LOCATION_DISTANCE_DEFAULT = 50f;  //
+    public static final int LOCATION_INTERVAL_DEFAULT = 600000;  // 10 min
+    public static final float LOCATION_DISTANCE_DEFAULT = 600f;  //
 
     private LocationCallback mCallback;
     private Location mLastLocation = new Location("");
-    private LocationManager mLocationManager = null;
     private Location mDestiny;
+    private GoogleApiClient mGoogleApiClient;
+    private LocationRequest mLocationRequest;
 
     private static final String TAG = LocationService.class.getSimpleName();
     private static boolean isActiveAlive = false;
@@ -45,13 +50,12 @@ public class LocationService extends Service {
     private Notifications mNotifications;
 
 
-
     public Location getDestiny() {
         return mDestiny;
     }
 
     public void setDestiny(LatLng poLatLng) {
-        if(poLatLng==null){
+        if (poLatLng == null) {
             mDestiny = null;
         } else {
             this.mDestiny = new Location("");
@@ -62,7 +66,7 @@ public class LocationService extends Service {
 
     public void onActivityLive(boolean poActivityLive) {
         isActiveAlive = poActivityLive;
-        if(isActiveAlive){
+        if (isActiveAlive) {
             mNotifications.prcRemoveNotificationStatus();
         } else {
             if (getDestiny() != null) mNotifications.prcStatus(mRules.getText());
@@ -70,13 +74,13 @@ public class LocationService extends Service {
     }
 
     public void getDistance() {
-        if (getDestiny() != null){
+        if (getDestiny() != null) {
             mRules.setDistance(mLastLocation.distanceTo(getDestiny()));
             Log.e(TAG, "distance: " + mRules.getDistance());
-            //prcUpdateIntervals(mRules.getDistance());
-            if(isActiveAlive && mCallback!=null){
-                Log.e(TAG, "mCallback: "+ (mCallback==null)+"");
-                Log.e(TAG, "mRules: " + (mRules==null)+"");
+            prcUpdateIntervals(mRules.getDistance());
+            if (isActiveAlive && mCallback != null) {
+                Log.e(TAG, "mCallback: " + (mCallback == null) + "");
+                Log.e(TAG, "mRules: " + (mRules == null) + "");
                 mCallback.onDistance(mRules);
             } else {
                 prcShowNotification();
@@ -84,23 +88,76 @@ public class LocationService extends Service {
         }
     }
 
+
     private void prcUpdateIntervals(double poDistance) {
-        if(poDistance > RulesVO.MAX_VALUE){
+        if (poDistance < RulesVO.MAX_VALUE) {
             Log.e(TAG, " *** poDistance > RulesVO.MAX_VALUE *** ");
-            if(mRules.LOCATION_INTERVAL != LOCATION_INTERVAL){
+
+            Log.e(TAG, " *** mRules.LOCATION_INTERVAL: "+mRules.LOCATION_INTERVAL);
+            Log.e(TAG, " *** LOCATION_INTERVAL: "+ LOCATION_INTERVAL);
+
+            if (mRules.LOCATION_INTERVAL != LOCATION_INTERVAL) {
+                LOCATION_INTERVAL = mRules.LOCATION_INTERVAL;
+                LOCATION_DISTANCE = mRules.LOCATION_DISTANCE;
                 Log.e(TAG, " *** Cambie los listeners *** ");
-                mRules.LOCATION_DISTANCE = LOCATION_DISTANCE;
-                mRules.LOCATION_INTERVAL = LOCATION_INTERVAL;
-                Log.e(TAG, " *** LOCATION_DISTANCE: "+LOCATION_DISTANCE);
-                Log.e(TAG, " *** LOCATION_INTERVAL: "+LOCATION_INTERVAL);
-                prcRemoveLister();
-                prcCreateListeners();
+                mLocationRequest.setInterval(mRules.LOCATION_INTERVAL);
+                mLocationRequest.setSmallestDisplacement(mRules.LOCATION_DISTANCE);
+
+                Log.e(TAG, " *** LOCATION_DISTANCE: " + mRules.LOCATION_DISTANCE);
+                Log.e(TAG, " *** LOCATION_INTERVAL: " + mRules.LOCATION_INTERVAL);
+                startLocationUpdates();
             }
         }
     }
 
+
     private void prcShowNotification() {
         mNotifications.prcStatus(mRules.getText());
+    }
+
+    public void getLastPosition() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+        mCallback.onLocationChange(new LatLng(mLastLocation.getLatitude(), mLastLocation.getLongitude()));
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        startLocationUpdates();
+    }
+
+    private void startLocationUpdates() {
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
+    }
+
+    protected void stopLocationUpdates() {
+        LocationServices.FusedLocationApi.removeLocationUpdates(mGoogleApiClient, this);
+    }
+
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        isChangePosition = true;
+        mLastLocation.set(location);
+        if (mCallback != null)
+            mCallback.onLocationChange(new LatLng(mLastLocation.getLatitude(), mLastLocation.getLongitude()));
+        getDistance();
     }
 
     public class LocalBinder extends Binder {
@@ -112,7 +169,7 @@ public class LocationService extends Service {
     }
 
     public LatLng getPosition() {
-        return (isChangePosition) ? new LatLng(mLastLocation.getLatitude(), mLastLocation.getLongitude()): null;
+        return (isChangePosition) ? new LatLng(mLastLocation.getLatitude(), mLastLocation.getLongitude()) : null;
     }
 
     @Override
@@ -136,7 +193,7 @@ public class LocationService extends Service {
 
     @Override
     public void onCreate() {
-        Log.e(TAG, "onCreate "+isActiveAlive);
+        Log.e(TAG, "onCreate " + isActiveAlive);
         isActiveAlive = false;
         init();
     }
@@ -144,110 +201,43 @@ public class LocationService extends Service {
     private void init() {
         Log.e(TAG, "init");
 
-        initializeLocationManager();
-        prcCreateListeners();
+        mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(LOCATION_INTERVAL);
+        mLocationRequest.setSmallestDisplacement(LOCATION_DISTANCE);
+        mLocationRequest.setFastestInterval(5000);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
+
         mRules = new RulesVO(getApplication());
         mNotifications = new Notifications(getApplication());
         LatLng voDestiny = new MapsSP(getApplication()).getLastDestinyPosition();
-        if(voDestiny!=null){
+        if (voDestiny != null) {
             setDestiny(voDestiny);
         }
 
+        if (mGoogleApiClient == null) {
+            mGoogleApiClient = new GoogleApiClient.Builder(this)
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(new GoogleApiClient.OnConnectionFailedListener() {
+                        @Override
+                        public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+                        }
+                    })
+                    .addApi(LocationServices.API)
+                    .build();
+        }
+        mGoogleApiClient.connect();
+
     }
 
-    private void prcCreateListeners() {
-        Log.e(TAG, "Create Listener");
-        try {
-            mLocationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, LOCATION_INTERVAL, LOCATION_DISTANCE, mLocationListeners[0]);
-            mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, LOCATION_INTERVAL, LOCATION_DISTANCE, mLocationListeners[1]);
-        } catch (java.lang.SecurityException ex) {
-            Log.i(TAG, "fail to request location update, ignore", ex);
-        } catch (IllegalArgumentException ex) {
-            Log.d(TAG, "network provider does not exist, " + ex.getMessage());
-        }
-    }
 
     @Override
     public void onDestroy() {
         Log.e(TAG, "onDestroy");
+        mGoogleApiClient.disconnect();
         super.onDestroy();
-        prcRemoveLister();
     }
 
-    private void prcRemoveLister() {
-        Log.e(TAG, "Remove Listener");
-        if (mLocationManager != null) {
-            for (int i = 0; i < mLocationListeners.length; i++) {
-                try {
-                    mLocationManager.removeUpdates(mLocationListeners[i]);
-                } catch (Exception ex) {
-                    Log.i(TAG, "fail to remove location listners, ignore", ex);
-                }
-            }
-        }
-    }
-
-    /////// Location methodss
-
-    private void initializeLocationManager() {
-        Log.e(TAG, "initializeLocationManager");
-        if (mLocationManager == null) {
-            mLocationManager = (LocationManager) getApplicationContext().getSystemService(Context.LOCATION_SERVICE);
-        }
-    }
-
-    private class LocationListener implements android.location.LocationListener {
-
-        public LocationListener(String provider) {
-            Log.e(TAG, "LocationListener " + provider);
-        }
-
-        @Override
-        public void onLocationChanged(Location location) {
-            Log.e(TAG, "Lat:" + location.getLatitude() +" Long:" +location.getLongitude());
-            Log.e(TAG, "isActiveAlive: " + isActiveAlive);
-            isChangePosition = true;
-            mLastLocation.set(location);
-            if(mCallback!=null) mCallback.onLocationChange(new LatLng(mLastLocation.getLatitude(), mLastLocation.getLongitude()));
-            getDistance();
-        }
-
-
-        @Override
-        public void onProviderDisabled(String provider) {
-            Log.e(TAG, "onProviderDisabled: " + provider);
-            if (mCallback != null) mCallback.onProviderDisabled(provider);
-        }
-
-        @Override
-        public void onProviderEnabled(String provider) {
-            Log.e(TAG, "onProviderEnabled: " + provider);
-        }
-
-        @Override
-        public void onStatusChanged(String provider, int status, Bundle extras) {
-            Log.e(TAG, "onStatusChanged: " + provider);
-        }
-    }
-
-    public static boolean isAppRunning(final Context context, final String packageName) {
-        final ActivityManager activityManager = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
-        final List<ActivityManager.RunningAppProcessInfo> procInfos = activityManager.getRunningAppProcesses();
-        if (procInfos != null)
-        {
-            for (final ActivityManager.RunningAppProcessInfo processInfo : procInfos) {
-                if (processInfo.processName.equals(packageName)) {
-                    return true;
-                }
-            }
-        }
-        return false;
-    }
-
-    LocationListener[] mLocationListeners = new LocationListener[]{
-            new LocationListener(LocationManager.NETWORK_PROVIDER),
-            new LocationListener(LocationManager.GPS_PROVIDER)
-    };
 
     public interface LocationCallback {
         void onLocationChange(LatLng poLocation);
